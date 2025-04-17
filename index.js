1b822ecb027d3d33c7d02ed2db59b9421b0bfb7d
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-04-17 04:57:03 +0100

    fs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/* const http = require('http');
 
 const PORT = 4000;
 const server = http.createServer((req, res) => {
@@ -9,4 +9,16 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, (err) => {
     console.log(`server is listening on port ${PORT}`)
     if (err) throw err
-})
+}) */
+
+// import fs from "fs"  // deuxième méthode d'importation de 'fs'   
+try {
+    const fs = require("fs");
+    const fileInput = fs.readFileSync(`input.txt`, "utf-8");
+    const message = fileInput.toLocaleLowerCase();
+    console.log(message);
+    fs.writeFileSync("output.txt", message);
+    console.log(message);
+} catch (error) {
+    console.log('Il y a une erreur\n ${error}');
+}
