ec759779e34ad1614335fd35d4030783e7f58abd
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-04-21 16:27:25 +0100

    utilisation-api

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ try {
 
 const express = require("express")
 const morgan = require("morgan")
-const favicon = require("serve-favicon")
+// const favicon = require("serve-favicon")
+const bodyParser = require("body-parser")
 const {success, getUniqueId} = require("./helper.js")
 let pokemons = require("./mock-pokemon")
 
@@ -35,6 +36,7 @@ const port = 3000
 
 app
   // .use(favicon[__dirname + "/favicon.ico"])  
+  .use(bodyParser.json()) // pour parser le corps de la requête
   .use(morgan('dev')) 
 
 /* app.use((req, res , next) => {
@@ -63,12 +65,44 @@ app.get("/api/pokemons/:id", (req, res) => {
 })
 
 app.post("/api/pokemons", (req, res) => {
-  const id = getUniqueId 
+  const id = getUniqueId(pokemons)
   const pokemonCreated = { ...req.body, ...{id: id, created: new Date()}}
   pokemons.push(pokemonCreated)
   const message = `Le pokemon ${pokemonCreated.name} a bien été crée`
   res.json(success(message, pokemonCreated))
 })
+
+app.put("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonUpdated = { ...req.body, id: id}
+  pokemons = pokemons.map(pokemon => {
+    return pokemon.id === id ? pokemonUpdated : pokemon
+  })
+  const message = `Le pokemon ${pokemonUpdated.name} a bien été modifié.`
+  res.json(success(message, pokemonUpdated))
+})  
+
+app.delete('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id);
+
+  if (!pokemonDeleted) {
+    const message = `Aucun Pokémon avec l'id ${id} n'a été trouvé.`;
+    return res.status(404).json({ success: false, message });
+  }
+
+  pokemons = pokemons.filter(pokemon => pokemon.id !== id); // Met à jour le tableau en excluant le Pokémon supprimé
+  const message = `Le Pokémon ${pokemonDeleted.name} a bien été supprimé.`;
+  res.json(success(message, pokemonDeleted));
+});
+
+/* app.delete('api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+  pokemons.filter(pokemon => pokemon.id !== id)
+  const message = `Le pokemon ${pokemonDeleted.name} a bien été supprimé`
+  res.json(success(message, pokemonDeleted))
+}) */ 
 
 /* app.get('/api/pokemons/', (req, res) => {
     res.send(`Il y a ${pokemons.length} pokemon en tout`)
--- mock-pokemon.js
+++ mock-pokemon.js
@@ -1,20 +1,52 @@
 const pokemons = [
     {
         id: 1,
-        name:"tottot"
+        name: "tottot",
+        type: "junior",
+        level: 1,
+        life: 100,
+        attack: 10,
+        defense: 5,
+        speed: 15,
+        specialAttack: 20,
+        specialDefense: 10
     },
     {
         id: 2,
-        name:"tudator"
+        name: "tudator",
+        type: "senior",
+        level: 2,
+        life: 120,
+        attack: 14,
+        defense: 8,
+        speed: 18,
+        specialAttack: 25,
+        specialDefense: 12
     },
     {
         id: 3,
-        name:"kamopat"
+        name: "kamopat",
+        type: "junior",
+        level: 1,
+        life: 90,
+        attack: 11,
+        defense: 6,
+        speed: 12,
+        specialAttack: 17,
+        specialDefense: 9
     },
     {
         id: 4,
-        name:"libona"
+        name: "libona",
+        type: "junior",
+        level: 1,
+        life: 95,
+        attack: 13,
+        defense: 4,
+        speed: 14,
+        specialAttack: 19,
+        specialDefense: 11
     }
-]
+];
 
 module.exports = pokemons;
